6de086ccfd38f9ca93194f38152c6a634a30ff6a
Diana-mar <marikyan2001@mail.ru>
2022-06-25 21:36:11 +0400

    CORS error fix

--- app.js
+++ app.js
@@ -2,7 +2,12 @@ let http = require("http")
 let port = 8000
 
 let listener = (req, res) => {
-    res.end("Test")
+        const headers = {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "OPTIONS, POST, GET"
+        }
+    res.writeHead(200, headers);
+    res.end('{"k":"Test"}') 
 }
 
 let server = http.createServer(listener)
