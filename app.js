b7fcf6fa7acec15e3d83390078ce3ac25c900a11
Diana-mar <marikyan2001@mail.ru>
2022-06-28 20:54:01 +0400

    uploaded version

--- app.js
+++ app.js
@@ -7,9 +7,14 @@ let listener = (req, res) => {
           "Access-Control-Allow-Methods": "OPTIONS, POST, GET"
         }
     res.writeHead(200, headers);
+/**/
+    res.write('<form action="fileupload" method="post" enctype="multipart/form-data">')
+    res.write('<input type="file" id="pic" accept=".png, .jpg, .jpeg"><br>')
+    res.write('<input type="button" id="btn" value="Submit">')
+    res.write('</form>')
+/**/
     res.end('{"k":"Test"}') 
 }
-
 let server = http.createServer(listener)
 server.listen(port)
 console.log("Server is up and running on port", port)
